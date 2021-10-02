6a5c427ba310d3fb148a81e985564081bf9702b5
howmanysmall <lhamil29@bruinmail.slcc.edu>
2021-10-02 10:57:00 -0600

    V1.13.11 actual release.

--- src/Symbol.lua
+++ src/Symbol.lua
@@ -1,5 +1,5 @@
 -- This only exists because the LSP warns Key `__tostring` not found in type `table?`.
-local function Symbol(Name)
+local function Symbol(Name: string)
 	local self = newproxy(true)
 	local Metatable = getmetatable(self)
 	function Metatable.__tostring()
